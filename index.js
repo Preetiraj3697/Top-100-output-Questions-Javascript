d2a15df9ab06dd301407faa42c3fc50ab4077755
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-02-26 18:53:01 +0530

    add

--- index.js
+++ index.js
@@ -7,9 +7,27 @@
 // console.log(a===b); // false
 //==============================//
 //2
-let a = [];
-let b = a;
-console.log(a==b); // true
-console.log(a===b); // true
+// let a = [];
+// let b = a;
+// console.log(a==b); // true
+// console.log(a===b); // true
 //because of memory location
 //==============================//
+//3
+// let a = [20];
+// let b = [20];
+// console.log(a[0]==b[0]); // true
+// console.log(a[0]===b[0]); // true
+//because of memory location
+//==============================//
+//4
+// let z = [1,2,3,4]
+// let a = {name:"anil"};
+// console.log(...z); // 1,2,3,4
+//==============================//
+//5
+// console.log(typeof NaN); //number
+//==============================//
+//6
+let data = 10 - -10;
+console.log(data); //20
