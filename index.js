621911a59f607c975356c62396f79a852dc0237b
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-02-26 18:43:00 +0530

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("hello world")
+//===============================//
+//1
+// let a = [];
+// let b = [];
+// console.log(a==b); // false
+// console.log(a===b); // false
+//==============================//
+//2
+let a = [];
+let b = a;
+console.log(a==b); // true
+console.log(a===b); // true
+//because of memory location
+//==============================//
