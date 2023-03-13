2dd6657c0a9cab2e053a608f1dfccfc0bebcd38b
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-03-13 09:19:21 +0530

    add copyWithin

--- Array Methods/copyWithin.js
+++ Array Methods/copyWithin.js
@@ -11,3 +11,22 @@
 let sports = ["cricket","Football","Chess","Hockey"];
 sports.copyWithin(2,0);
 console.log('sports: ', sports); // ['cricket', 'Football', 'cricket', 'Football']
+
+let sports1 = ["cricket","Football","Chess","Hockey","Badminton","Tennis"];
+sports1.copyWithin(2,0,2);
+console.log('sports1: ', sports1); // ['cricket', 'Football', 'cricket', 'Football', 'Badminton', 'Tennis']
+sports1.copyWithin(2,0);
+console.log('sports1: ', sports1); // ['cricket', 'Football', 'cricket', 'Football', 'cricket', 'Football']
+
+
+const num = [1,2,3,4,5]; // [1,2,3,3,5];
+num.copyWithin(-2,-3,-1);
+console.log('num: ', num); // [1, 2, 3, 3, 4]
+// num.copyWithin(-2,-4);
+// console.log('num: ', num); // [1, 2, 3, 2, 3]
+// num.copyWithin(-5,-1);
+// console.log('num: ', num); // [5, 2, 3, 4, 5]
+// num.copyWithin(1,4);
+// console.log('num: ', num); // [1, 5, 3, 4, 5]
+
+
