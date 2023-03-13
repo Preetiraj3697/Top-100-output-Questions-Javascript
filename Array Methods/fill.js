33fa2f953298f3d8b2ddc0cc982f6d03b7d25543
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-03-13 09:37:33 +0530

    add fill

---
+++ Array Methods/fill.js
@@ -0,0 +1,11 @@
+// fill();
+// The fill() method fills the specified elements in an array with a static value. You can specify the position
+// of where to start and end the filling. if not specified, all elements will be filled.
+// Syntax:= array.fill(value,start,end);
+// value = The value to fill the array with
+// start (optional) = The index to start filling the array (default is 0);
+// end (optional) = The index to stop filling the array (default is array.length);
+
+// Key Point
+// Method overwrites the original array
+// Method is not supported in Internet Explorer 11 and earlier versions.
