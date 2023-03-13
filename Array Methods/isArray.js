c3a7e6656f46083af5c86c63f13b39f0aa2cb545
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-03-13 16:23:33 +0530

    add isArray

---
+++ Array Methods/isArray.js
@@ -0,0 +1,12 @@
+// isArray();
+
+// The isArray() method determines whether an object is an array. This function returns true if the object
+// is an array, and false if not.
+
+// Syntax := Array.isArray(obj);
+
+// obj(Required) := The object to be tested.
+
+// Array.isArray();
+console.log(Array.isArray([1,2,3])) // true
+console.log(Array.isArray({abc:123}))  // false
