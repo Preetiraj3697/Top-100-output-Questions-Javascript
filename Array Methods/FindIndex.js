78adef0d163ffa09c9755a558016f8ab57b57dc5
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-03-13 10:42:05 +0530

    add FindIndex

---
+++ Array Methods/FindIndex.js
@@ -0,0 +1,13 @@
+// findIndex();
+// The findIndex() method returns the index of the first element in an array that pass a test (provided as a function).
+// if if finds an array element where the function returns a true value, findIndex() returns the index of that array
+// element (and does not check the remaning values) otherwise it returns -1.
+// Syntax :- array.findIndex(function(currentValue,index,arr),thisValue);
+// value = The value of the current element
+// index (optional) = The array index of the current element
+// arr (optional) = The array object the current element belongs to
+// thisValue (optional) = A value to be passed to the function to be used as its "this" value. If this
+// parameter is empty, the value "undefined" will be passed as its "this" value
+// Key Point
+// findIndex() does not execute the function for empty arrays.
+// findIndex() does not change the original array.
