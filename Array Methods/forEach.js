63c140dbb885b5d910a62d9fbab98d7ac7e714c1
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-03-13 11:31:18 +0530

    forEach

---
+++ Array Methods/forEach.js
@@ -0,0 +1,13 @@
+// forEach()
+// The forEach() method calls a function once for each element in an array, in order.
+
+// Syntax:- array.forEach(function(currentValue,index,arr),thisValue);
+
+// CurrentValue (Required) = The value of the current element
+// Index (Optional) = The array index of the current element
+// arr (Optional) = The array object the current element belongs to 
+// thisValue(Optional) = A value to be passed to the function to be used as its "this" value. if this parameter is empty,
+// the value "undefined" will be passed as its "this" value.
+// Key Point:=
+// forEach expects a synchronous function
+// forEach does not wait for promises.
