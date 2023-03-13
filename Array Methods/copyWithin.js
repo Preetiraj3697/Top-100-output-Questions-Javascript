67c7dc631f29d74be113f5caf0a0fdb981fe0853
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-03-13 09:05:35 +0530

    add copyWithin

---
+++ Array Methods/copyWithin.js
@@ -0,0 +1,9 @@
+// copyWithin();
+// The copyWithin() method copies array elements to another position in the array, overwriting the existing values.
+// Syntax := array.copyWithin(target,start,end);
+// target = index position to copy element
+// start (optional) = index positioin to start copying element (default is 0);
+// end (optional) = index position to stop copying element (default is array length);
+// Key point =>
+// Method will never add more item to array
+// Method overwrites the original array
