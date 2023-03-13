79b7b933d11f796359ed793b89a0c52ba2c4b2fa
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-03-13 09:46:49 +0530

    add filter

---
+++ Array Methods/Filter.js
@@ -0,0 +1,13 @@
+// filter();
+// The filter() method creates an array filled with all array elements that pass a test (provided as a function);
+// Syntax:- array.filter(function(currentValue,index,arr),thisValue);
+// value = The value of the current Element
+// index(optional) = The array index of the current element
+// arr(optional) = The array object the current element belongs to
+// thisValue (optional) = A value to be passed to the function to be used as its "this" value. if this parameter is empty.
+// the value "undefined" will be passed as its "this" value. 
+
+// Key point
+// filter() does not execute the function for array elements without values.
+// filter() does not change the original array.
+
